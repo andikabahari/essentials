be12d61baa87cdf66c1f878390ee86e8d959a5e2
andikabahari <andikabahari48@gmail.com>
2026-04-17 20:11:17 +0700

    Don't ignore test_all.cpp

---
+++ test/test_all.cpp
@@ -0,0 +1,110 @@
+#ifndef BASE_COMPILED
+#define BASE_IMPLEMENTATION
+#endif
+#include "../base.h"
+
+#pragma push_macro("internal")
+#undef internal
+
+#ifndef GFX_COMPILED
+#define GFX_IMPLEMENTATION
+#endif
+#include "../gfx.h"
+
+#include <stdio.h>
+#include <SDL3/SDL.h>
+
+#pragma pop_macro("internal")
+
+#define TEST(name) void name()
+#define RUN_TEST(name) do { name(); printf("[OK] %s\n", #name); } while (0)
+
+internal Arena *make_arena() {
+    return arena_create(MiB(16), MiB(1));
+}
+
+#include "test_base.cpp"
+#include "test_gfx.cpp"
+
+int main() {
+    //
+    // base.h test cases
+    //
+    
+    RUN_TEST(test_defer_basic);
+    RUN_TEST(test_defer_lifo);
+    RUN_TEST(test_defer_nested);
+    RUN_TEST(test_defer_return);
+
+    RUN_TEST(test_arena_push_one);
+    RUN_TEST(test_arena_push_many);
+    RUN_TEST(test_arena_zero_flag);
+    // RUN_TEST(test_arena_pop); // Test will fail due to alignment!
+    RUN_TEST(test_arena_pop_to);
+    RUN_TEST(test_arena_clear);
+    RUN_TEST(test_arena_temp_arena);
+    RUN_TEST(test_arena_nested_temp);
+    RUN_TEST(test_arena_alignment);
+
+    RUN_TEST(test_scratch_basic);
+    RUN_TEST(test_scratch_reset);
+    RUN_TEST(test_scratch_conflict);
+    RUN_TEST(test_scratch_multiple_conflicts);
+    RUN_TEST(test_scratch_nested);
+    RUN_TEST(test_scratch_reuse);
+
+    RUN_TEST(test_thread_local_scratch);
+    RUN_TEST(test_thread_local_reuse);
+    RUN_TEST(test_thread_stress);
+
+    RUN_TEST(test_array_add);
+    RUN_TEST(test_array_reserve);
+    RUN_TEST(test_array_growth);
+    RUN_TEST(test_array_pop);
+    RUN_TEST(test_array_clear);
+    RUN_TEST(test_array_ordered_remove);
+    RUN_TEST(test_array_unordered_remove);
+
+    RUN_TEST(test_string_eq);
+    RUN_TEST(test_string_compare);
+    RUN_TEST(test_string_index);
+    RUN_TEST(test_string_prefix_suffix);
+    RUN_TEST(test_string_cut);
+    RUN_TEST(test_string_trim_prefix_suffix);
+    RUN_TEST(test_string_trim_space);
+    RUN_TEST(test_string_split);
+    RUN_TEST(test_string_split_empty);
+    RUN_TEST(test_string_join);
+    RUN_TEST(test_string_concat);
+    RUN_TEST(test_string_replace);
+    RUN_TEST(test_string_case);
+    RUN_TEST(test_string_ops_string);
+    RUN_TEST(test_string_ops_cstr);
+    RUN_TEST(test_string_ops_empty_cstr);
+    RUN_TEST(test_string_ops_edge_cases);
+    RUN_TEST(test_string_ops_length);
+
+    RUN_TEST(test_table_basic);
+    RUN_TEST(test_table_overwrite);
+    RUN_TEST(test_table_collision);
+    RUN_TEST(test_table_delete);
+    RUN_TEST(test_table_tombstone_reuse);
+    RUN_TEST(test_table_grow);
+    RUN_TEST(test_table_stress);
+    RUN_TEST(test_table_basic_string);
+    RUN_TEST(test_table_overwrite_string);
+    RUN_TEST(test_table_collision_string);
+    RUN_TEST(test_table_delete_string);
+    RUN_TEST(test_table_content_eq_string);
+    RUN_TEST(test_table_stress_string);
+    RUN_TEST(test_table_clear);
+
+    //
+    // gfx.h test cases
+    //
+
+    RUN_TEST(test_gfx);
+
+    printf("All tests passed!\n");
+    return 0;
+}
